536c4fedf379ee3e749e0ed2ab924a4ed4189244
NakaiKenta <nk19940709nk@gmail.com>
2024-08-24 20:37:34 +0900

    add log message

--- library/src/main/java/com/company/library/primary/controller/TestController.java
+++ library/src/main/java/com/company/library/primary/controller/TestController.java
@@ -1,13 +1,16 @@
 package com.company.library.primary.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
 
     @GetMapping("/nakai")
     public String test() {
+        log.info("this is test log message!");
         return "this is test message";
     }
 }
